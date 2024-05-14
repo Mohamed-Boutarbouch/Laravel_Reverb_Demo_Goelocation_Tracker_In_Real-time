f4b864d2add948996ca3f19c794f880546788a8c
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-14 22:41:32 +0100

    display courier's coords to the admin

--- resources/js/Components/AdminMap.tsx
+++ resources/js/Components/AdminMap.tsx
@@ -1,26 +1,53 @@
-import { useRef } from "react";
-import { MapContainer } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { useEffect, useRef, useState } from "react";
+import { MapContainer, Marker, Popup } from "react-leaflet";
+import L, { LatLngExpression } from "leaflet";
 
 import { MapDisplay } from "./MapDisplay";
+import courierIcon from "../../../public/courier.png";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import { echo } from "@/echo";
+
+type PayloadCoords = { lat: number; lng: number };
 
 export function AdminMap() {
   const mapRef = useRef(null);
   const CENTER: LatLngExpression = [34.00501829905671, -4.957129955291749];
+  const [courierCoords, setCourierCoords] = useState<PayloadCoords | null>({
+    lat: 34.00501829905671,
+    lng: -4.957129955291749,
+  });
 
-  console.log(mapRef.current);
+  console.log(courierCoords);
+
+  useEffect(() => {
+    echo
+      .private("geolocation")
+      .listen("CoordinateSent", (payload: PayloadCoords) => {
+        setCourierCoords(payload);
+      });
+  }, []);
+
   return (
     <MapContainer
-      center={CENTER}
+      center={courierCoords ? [courierCoords?.lat, courierCoords?.lng] : CENTER}
       zoom={16}
       ref={mapRef}
       style={{ height: "600px", width: "100%" }}
     >
       <MapDisplay />
+      {courierCoords && (
+        <Marker
+          position={[courierCoords.lat, courierCoords.lng]}
+          icon={L.icon({ iconUrl: courierIcon, iconSize: [70, 70] })}
+        >
+          <Popup>
+            Courier Location: {courierCoords.lat}, {courierCoords.lng}
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
